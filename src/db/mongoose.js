171e0e1cccb8ded1f321dcc8d0e253ed0c79b8a0
Alyona <alyona.rodin@gmail.com>
2020-08-23 22:43:43 -0400

    added endpoints

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
+
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
     useUnifiedTopology: true,
@@ -7,68 +7,16 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
     useCreateIndex:true
 })
 
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email:{
-        type: String,
-        required: true,
-        trim:true,
-        lowercase:true,
-        validate(value){
-            if(!validator.isEmail(value)){
-                throw new Error('Email is invalid')
-            }
-        }
-    },
-    age:{
-        type: Number,
-        default:0,
-        validate(value){
-            if(value<0){
-                throw new Error('Age must be a positive number')
-            }
-        }
-    },
-    password:{
-        type: String,
-        required:true,
-        trim:true, 
-        minlength:7,
-        validate(value){
-            if(value.toLowerCase().includes('password')){
-                throw new Error('Password cannot contain "password"')
-            }
-        }
-    }
+// const myTask = new Task({
+//     description:'Task4',
+//     completed: true
+// })
 
-})
-
-const Task =mongoose.model('Task',{
-    description:{
-        type: String,
-        trim: true,
-        required: true
-    },
-    completed:{
-        type: Boolean,
-        default: false
-    }
-})
-
-const myTask = new Task({
-    description:'Task4',
-    completed: true
-})
-
-myTask.save().then((myTask)=>{
-    console.log(myTask)
-}).catch((error)=>{
-    console.log('Error',error)
-})
+// myTask.save().then((myTask)=>{
+//     console.log(myTask)
+// }).catch((error)=>{
+//     console.log('Error',error)
+// })
 
 // const me = new User({
 //     name:'   Tal   ',
