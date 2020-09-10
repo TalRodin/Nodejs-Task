70383ec76afaa51d0f8526a319a05e53f7fdd54f
Alyona <alyona.rodin@gmail.com>
2020-09-09 20:44:55 -0400

    first commit

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt =require('bcryptjs')
-
+const jwt =require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({name: {
     type: String,
@@ -29,6 +29,12 @@ age:{
         }
     }
 },
+tokens:[{
+    token:{
+        type:String,
+        required:true,
+    }
+}],
 password:{
     type: String,
     required:true,
@@ -41,6 +47,17 @@ password:{
     }
 }
 })
+
+userSchema.methods.generateAuthToken = async function (){
+    const user = this
+    const token = jwt.sign({_id: user._id.toString()}, 'thisismynewcourse')
+    
+    user.tokens = user.tokens.concat({ token})
+    await user.save()
+    return token
+}
+
+
 
 userSchema.statics.findByCredentials = async(email, password)=>{
     const user = await User.findOne({email})
